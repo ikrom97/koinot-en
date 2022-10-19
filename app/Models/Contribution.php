087ee3a75923f6fd355b3963c181c06e5662b4ad
ikrom97 <ikromr04@gmail.com>
2022-10-19 20:09:28 +0500

    first commit

---
+++ app/Models/Contribution.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contribution extends Model
+{
+  use HasFactory;
+
+  protected $guarded = [];
+
+  public function images()
+  {
+    return $this->hasMany(Image::class, 'contribution_id');
+  }
+}
---
+++ app/Models/News.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+  use HasFactory;
+
+  protected $guarded = [];
+
+  public function images()
+  {
+    return $this->hasMany(Image::class, 'news_id');
+  }
+}
