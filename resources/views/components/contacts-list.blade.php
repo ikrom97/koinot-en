ba90361aeb071e556aa58af15026a2645ce50dc2
ikrom97 <ikromr04@gmail.com>
2022-12-30 14:39:34 +0500

    email change

--- resources/views/components/contacts-list.blade.php
+++ resources/views/components/contacts-list.blade.php
@@ -6,7 +6,7 @@
   <li class="contacts-list__item">
     <h3 class="contacts-list__title">General questions</h3>
     <a class="contacts-list__phone" href="tel:+992446000043">+992 (44) 600 00 43</a>
-    <a class="contacts-list__email" href="mailto:info@koinotinav.tj">info@koinotinav.tj</a>
+    <a class="contacts-list__email" href="mailto:info@koinotinav.tj">info@koinotinav.com</a>
     <span class="contacts-list__icon">
       <svg width="16" height="19">
         <use xlink:href="#file"></use>
